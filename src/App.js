68a05dab1a4e47090e6dfd2465d4f642cdd5bcae
rushiwadkar007 <rwadkar920@gmail.com>
2022-01-16 15:36:10 +0530

    files added.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import react from 'react';
+
+const App = () => {
+
+    return (
+
+        <div>
+
+            <h1>
+
+                Hello There, I am Rushi here to learn from you React.js
+
+            </h1>
+
+        </div>
+
+    )
+
+}
